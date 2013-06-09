cdb62eadce0a3c5efd6528419e3907baeb14e665
spajky <marinspajic17@hotmail.com>
2013-06-09 03:47:15 +0200

    pico -> golfu
    You don't know how to kangbang properly. OMFG.

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,7 +18,7 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
-#define BTM_DEF_LOCAL_NAME   "HTC Explorer A310e"
+#define BTM_DEF_LOCAL_NAME   "HTC Desire C"
 #define BTA_DISABLE_DELAY 1000 /* in milliseconds */
 
 #endif
